e3e3cb5c20c04014da0df75f001eaf9818afbdfb
romisfrag <romisfrag@gmail.com>
2016-11-17 20:37:34 +0100

    add trieshybride

--- src/patriciaTries/Main.java
+++ src/patriciaTries/Main.java
@@ -7,6 +7,15 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		ParsingTexte t = new ParsingTexte("test/test.txt");
 		t.fullfillTries();
+		
+		
+		if("a".compareTo("b") < 0){
+			System.out.println("a".compareTo("b"));
+		}
+		else{
+			System.out.println("failure");
+		}
+		
 		return;
 	}
 
--- src/triesHybrides/TriesHybrides.java
+++ src/triesHybrides/TriesHybrides.java
@@ -4,24 +4,65 @@ import interfaces.ITries;
 
 public class TriesHybrides implements ITries{
 	
-	private String[] caracteres;
+	private char caractere;
+	private int valeur;
 	private TriesHybrides[] fils;
 	
 	/* macros */
 	public static final int GAUCHE = 0;
 	public static final int MILIEU = 1;
 	public static final int DROIT = 2;
-			
+
+	private static int compteur = 0;
+	
 	
 	public TriesHybrides(){
-		this.caracteres = new String[3];
+		this.caractere = (char)0;
+		this.valeur = -1;
+		/* au cas ou */
 		this.fils = new TriesHybrides[3];
 	}
 
 
 	@Override
 	public void insertion(String element) {
-				
+		
+		if(element == null){
+			return;
+		}
+		char premiereLettre = element.charAt(0);
+		
+		/* comme pour les patricia tries on prend le code ascii 0 comme caractere de null */
+		if (caractere == 0){			
+			caractere = premiereLettre;
+			
+			if(element.length() == 1){				
+				valeur = compteur++;
+			}
+			else{
+				fils[MILIEU].insertion(element.substring(1));				
+			}			
+		}
+		
+		
+		
+		else if(premiereLettre == caractere){			
+			if(element.length() == 1 && valeur == -1){
+				valeur = compteur++;				
+			}
+			else{
+				fils[MILIEU].insertion(element.substring(1));	
+			}
+			
+		}
+		
+		else if(premiereLettre < caractere){
+			fils[GAUCHE].insertion(element);
+		}
+		else{
+			fils[DROIT].insertion(element);
+		}
+		return;
 	}
 
 
@@ -34,7 +75,31 @@ public class TriesHybrides implements ITries{
 
 	@Override
 	public boolean recherche(String element) {
-		// TODO Auto-generated method stub
+		if(element == null){
+			return false;
+		}
+		char premiereLettre = element.charAt(0);
+		
+		if(premiereLettre == caractere){
+			
+			if(element.length() == 1){
+				if(compteur > 0){
+					return true;
+				}
+				return false;
+			}
+			fils[MILIEU].recherche(element.substring(1));
+		}
+		else if(premiereLettre < caractere){
+			fils[DROIT].recherche(element); 
+		}
+		else{
+			fils[GAUCHE].recherche(element);
+		}
+		
+		
+		
+		
 		return false;
 	}
 	
